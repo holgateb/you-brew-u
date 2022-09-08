37195f00b25048b910c59c335c0432faa3ce4402
Ben Holgate <holgateb@hotmail.com>
2022-09-08 15:51:41 -0700

    created post routes.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
-const postRoutes = require('./post-routes');
-const commentRoutes = require('./comment-routes');
+const recipeRoutes = require('./recipe-routes');
+// const commentRoutes = require('./comment-routes');
 
 
 router.use('/users', userRoutes);
-router.use('/post', postRoutes);
-router.use('/comment', commentRoutes);
+router.use('/recipe', recipeRoutes);
+// router.use('/comment', commentRoutes);
 
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,10 @@
 const User = require('./User');
+const Recipe = require('./Recipe')
 
 // Create associations
+Recipe.belongsTo(User, {
+    foreignKey: 'user_id',
+    
+})
 
-module.exports = { User };
+module.exports = { User, Recipe };
