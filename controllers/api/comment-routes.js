c8062d2ca762ab098c62404656551c8787e75924
Ben Holgate <holgateb@hotmail.com>
2022-09-07 21:07:34 -0700

    updated routes again.

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,25 +1,5 @@
 const router = require('express').Router();
 const { User, Comment, Post } = require('../../models');
 
-// CREATE new user
-router.post('/', async (req, res) => {
-    try {
-      const dbUserData = await User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-      });
-  
-      req.session.save(() => {
-        req.session.user_id = userData.id;
-        req.session.logged_in = true;
-  
-        res.status(200).json(dbUserData);
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-});
 
 module.exports = router;
